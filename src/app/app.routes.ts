d273f813d01ee2451212c26164d6d7d6b4be4419
Tirumala Rao  S <tiru@live.in>
2025-01-18 11:22:40 +0530

    fixednavigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ServicesComponent } from './components/services/services.component';
@@ -11,9 +10,3 @@ export const routes: Routes = [
     { path: 'services', component: ServicesComponent },
     { path: 'contact', component: ContactComponent },
 ];
-
-@NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-})
-export class AppRoutingModule { }
